8899ea05f0c51e5c57254e9ecf3e57dc913b84b6
henbsantos <rickesantoss@hotmail.com>
2024-09-12 20:33:27 -0300

    Arquivos IaC v.1

--- iac1.sh
+++ iac1.sh
@@ -9,19 +9,19 @@ groupadd GRP_VEN
 groupadd GRP_SEC
 
 #criando usuarios do grupo de adm e colocando em seu grupo
-useradd -m -s /bin/bash -G GRP_ADM -p $(openssl passwd -6 Senha123) carlos
-useradd -m -s /bin/bash -G GRP_ADM -p $(openssl passwd -6 Senha123) maria
-useradd -m -s /bin/bash -G GRP_ADM -p $(openssl passwd -6 Senha123) joao
+useradd -m -s /bin/bash -G GRP_ADM -p $(openssl passwd  Senha123) carlos
+useradd -m -s /bin/bash -G GRP_ADM -p $(openssl passwd  Senha123) maria
+useradd -m -s /bin/bash -G GRP_ADM -p $(openssl passwd  Senha123) joao
 
 #criando usuarios do grupo de ven e colocando em seu grupo
-useradd -m -s /bin/bash -G GRP_VEN -p $(openssl passwd -6 Senha123) debora
-useradd -m -s /bin/bash -G GRP_VEN -p $(openssl passwd -6 Senha123) sebastiana
-useradd -m -s /bin/bash -G GRP_VEN -p $(openssl passwd -6 Senha123) roberto
+useradd -m -s /bin/bash -G GRP_VEN -p $(openssl passwd  Senha123) debora
+useradd -m -s /bin/bash -G GRP_VEN -p $(openssl passwd  Senha123) sebastiana
+useradd -m -s /bin/bash -G GRP_VEN -p $(openssl passwd  Senha123) roberto
 
 #criando usuarios do grupo de sec e colocando em seu grupo
-useradd -m -s /bin/bash -G GRP_SEC -p $(openssl passwd -6 Senha123) josefina
-useradd -m -s /bin/bash -G GRP_SEC -p $(openssl passwd -6 Senha123) amanda
-useradd -m -s /bin/bash -G GRP_SEC -p $(openssl passwd -6 Senha123) rogerio
+useradd -m -s /bin/bash -G GRP_SEC -p $(openssl passwd  Senha123) josefina
+useradd -m -s /bin/bash -G GRP_SEC -p $(openssl passwd  Senha123) amanda
+useradd -m -s /bin/bash -G GRP_SEC -p $(openssl passwd  Senha123) rogerio
 
 #adicionado os grupos aos diretorios 
 chown root:GRP_ADM /adm
